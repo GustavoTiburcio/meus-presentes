4d044c536ddc160754f0c45aaf5783c5ce47b602
Gustavo Tiburcio <gustavotiburcio23@gmail.com>
2023-10-25 11:31:17 -0300

    favicon and minhas listas on userpanel

--- src/pages/UserPanel/index.tsx
+++ src/pages/UserPanel/index.tsx
@@ -1,7 +1,11 @@
 import { useState } from 'react';
 import IconeDinamico from '../../components/IconeDinamico';
 import {
+  ButtonOption,
+  ButtonsContainer,
   Container, InicioButtonsContainer, ItemMenu,
+  ListaPresenteContainer,
+  ListaPresenteTitle,
   MenuContainer, MenuInfoContainer, SubContainer,
 } from './styles';
 import { Button } from '../../components/Presentation/styles';
@@ -30,6 +34,8 @@ export default function UserPanel() {
 
     return (
       <MenuContainer>
+        <h2>Menu</h2>
+        <br />
         {itemsMenu.map((itemMenu, index) => (
           <ItemMenu
             onClick={() => {
@@ -39,7 +45,7 @@ export default function UserPanel() {
             key={index}
             isSelected={itemMenu.name === selectedItemMenu}
           >
-            <IconeDinamico nome={itemMenu.iconName} />
+            <IconeDinamico nome={itemMenu.iconName} size={25} />
             <span>{itemMenu.label}</span>
           </ItemMenu>
         ))}
@@ -60,7 +66,40 @@ export default function UserPanel() {
             </InicioButtonsContainer>
           </>
         </>,
-      minhasListas: <>Minhas Listas</>,
+      minhasListas:
+        <>
+          <h2>Minhas listas de presentes</h2>
+          <p>Veja suas listas de presente aqui.</p>
+          <ListaPresenteContainer>
+            <ListaPresenteTitle>
+              <IconeDinamico nome={'AiOutlineGift'} />
+              770756 - Chá de Casa Nova Gustavo
+            </ListaPresenteTitle>
+            <ButtonsContainer>
+              <ButtonOption>
+                <IconeDinamico nome='AiOutlineEdit' />
+                Editar Presentes
+              </ButtonOption>
+              <ButtonOption>
+                <IconeDinamico nome='AiOutlineEdit' />
+                Editar Informações
+              </ButtonOption>
+              <ButtonOption>
+                <IconeDinamico nome='AiOutlineFileExcel' />
+                Exportar p/ Excel
+              </ButtonOption>
+              <ButtonOption>
+                <IconeDinamico nome='AiOutlineShareAlt' />
+                Como Divulgar?
+              </ButtonOption>
+              <ButtonOption>
+                <IconeDinamico nome='AiOutlineDelete' />
+                Excluir Lista
+              </ButtonOption>
+            </ButtonsContainer>
+            <b>Endereço da Lista: <a href={window.location.origin + '/770756'} target='_blank'>{window.location.origin + '/770756'}</a></b>
+          </ListaPresenteContainer>
+        </>,
       contaDeUsuario: <>Conta de Usuário</>,
       privacidade: <>Privacidade</>,
       encerrarSessao: <>Encerrar Sessão</>,
--- src/pages/UserPanel/styles.ts
+++ src/pages/UserPanel/styles.ts
@@ -20,7 +20,7 @@ export const SubContainer = styled.div`
 export const MenuContainer = styled.div`
   width: 15%;
   /* background-color: cyan; */
-  border: 2px solid #E5E5E5;
+  /* border: 2px solid #E5E5E5; */
 
   div:hover{
     background: #F2F2F2;
@@ -32,7 +32,6 @@ export const MenuInfoContainer = styled.div`
   width: 80%;
   flex-direction: column;
   align-items: center;
-  /* background-color: magenta; */
 `;
 
 export const ItemMenu = styled.div<ItemMenuProps>`
@@ -40,13 +39,26 @@ export const ItemMenu = styled.div<ItemMenuProps>`
   align-items: center;
   padding: 0.5rem;
   cursor: pointer;
-  font-size: 1.5rem;
   background: ${({ isSelected }) => isSelected ? '#F2F2F2' : 'none'};
+
+  &:not(:first-child){
+    border: 2px solid #E5E5E5;
+    border-top: none;
+  }
+
+  &:nth-child(3){
+    border-top: 2px solid #E5E5E5;
+  }
+
+  svg {
+    width: 20%;
+  }
 
   span {
     margin-left: 1rem;
     font-weight: 450;
     font-size: 1.1rem;
+    width: 80%;
   }
 
 `;
@@ -59,4 +71,56 @@ export const InicioButtonsContainer = styled.div`
     margin-left: 1rem;
   }
 
+`;
+
+export const ListaPresenteContainer = styled.div`
+  margin: 1rem 0px;
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+  background: #F2F2F2;
+
+  > * {
+    margin: 1rem 0px 1rem 1rem;
+  }
+
+`;
+
+export const ListaPresenteTitle = styled.span`
+  display: flex;
+  align-items: center;
+  font-size: 1.2rem;
+  font-weight: 500;
+  text-indent: 0.5rem;
+
+  svg {
+    font-size: 1.8rem;
+  }
+`;
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  row-gap: 1rem;
+
+`;
+
+export const ButtonOption = styled.span`
+  display: flex;
+  align-items: center;
+  font-size: 1.2rem;
+  text-indent: 0.5rem;
+  cursor: pointer;
+
+  svg {
+    font-size: 1.8rem;
+  }
+
+  &:not(:first-of-type) {
+    margin-left: 1rem;
+  }
+  &:hover{
+    text-decoration: underline;
+  }
 `;
