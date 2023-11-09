f4d5a14d274cfc3ed79b56e02f33cf867a369cab
Gustavo Tiburcio <gustavotiburcio23@gmail.com>
2023-11-09 14:04:51 -0300

    NodeMailer config to use Zoho Mail SMTP

--- back-end/src/utils/index.ts
+++ back-end/src/utils/index.ts
@@ -21,16 +21,18 @@ export function isValidUUIDv4(uuid: string) {
 
 export function sendEmail(emailMessage: IEmailMessage) {
   const transporter = nodemailer.createTransport({
-    service: 'gmail',                              // the service used
+    host: 'smtp.zoho.com',
+    secure: true,
+    port: 465,
     auth: {
-      user: process.env.EMAIL_FROM,              // authentication details of sender, here the details are coming from .env file
+      user: process.env.EMAIL_FROM,
       pass: process.env.EMAIL_FROM_PASSWORD,
     },
   });
 
   const message = {
-    from: process.env.EMAIL_FROM,                         // sender email address
-    to: emailMessage.receiver,   // reciever email address
+    from: process.env.EMAIL_FROM,
+    to: emailMessage.receiver,
     subject: emailMessage.subject,
     html: emailMessage.body,
     // attachments: [
